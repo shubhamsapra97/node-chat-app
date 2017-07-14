d03db3dcb2b52bf4860726b33f70d3b9dd1f8df5
shubhamsapra97 <shubham.sapra97@gmail.com>
2017-07-14 20:05:54 +0530

    Add newMessage and createMessage Events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+//Arrow functions not used in js as arrow fn  will work on chrome but wont work on other browsers or mobile.
+socket.on('connect',function(){
+  console.log('Connected to Server');
+
+  socket.emit('createMessage' , {
+    from: 'sk@gmail.com',
+    text: 'Hello Bro!'
+  });
+
+});
+
+socket.on('disconnect' , function(){
+  console.log('Disconnected from Server');
+});
+
+socket.on('newMessage' , function(message){
+  console.log('New Message' , message);
+});
--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection' , (socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage' , {
+    from : 'shubham.sapra97@gmail.com',
+    message: 'hello Bro!',
+    createdAt: 123
+  });
+
+  socket.on('createMessage' , (message)=>{
+    console.log('Created Message' , message);
+  });
+
   socket.on('disconnect',()=>{
     console.log('Disconnected from Server');
   });
